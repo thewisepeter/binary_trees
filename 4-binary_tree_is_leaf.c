d14a2258b766e0b45da80fff1d141e87143067cc
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-01 22:32:51 +0300

    measures the height of a binary tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf 
+ * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
  *
  * Return: 1 is a leaf, 0 otherwise
@@ -9,6 +9,6 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node && !node->left && !node->right)
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
