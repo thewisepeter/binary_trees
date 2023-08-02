e6645db73fedb755075b8dd8953f77de7ae467c2
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-02 11:29:03 +0300

    fixed spell error in 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,7 +12,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
