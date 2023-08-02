c451da5bd8c75cab4627d7336741ee2e97eafd48
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-02 12:20:50 +0300

    refactored and made concise

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->right)
+	if (node == node->parent->left)
 		return (node->parent->right);
-	else if (node->parent->left)
-		return (node->parent->left);
-	else
-		return (NULL);
+	return (node->parent->left);
 }
