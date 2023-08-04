47d10b61a109d4dc07854d437988a0a132f244d2
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-04 22:38:38 +0300

    added helper func

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -9,10 +9,10 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    if (!tree)
-        return 0;
+	if (!tree)
+		return (0);
 
-    return is_complete_recursive(tree, 0, binary_tree_size(tree));
+	return (is_complete_recursive(tree, 0, binary_tree_size(tree)));
 }
 
 /**
@@ -23,15 +23,35 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  *
  * Return: 1 if the tree is complete, 0 otherwise
  */
-int is_complete_recursive(const binary_tree_t *tree, size_t index, size_t node_count)
+int is_complete_recursive(const binary_tree_t *tree,
+		size_t index, size_t node_count)
 {
-    if (!tree)
-        return 1;
+	if (!tree)
+		return (1);
 
-    if (index >= node_count)
-        return 0;
-
-    return (is_complete_recursive(tree->left, 2 * index + 1, node_count) &&
-            is_complete_recursive(tree->right, 2 * index + 2, node_count));
+	if (index >= node_count)
+		return (0);
+	return (is_complete_recursive(tree->left, 2 * index + 1, node_count) &&
+			is_complete_recursive(tree->right, 2 * index + 2, node_count));
 }
 
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ *
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+		return (0);
+
+	left_size = binary_tree_size(tree->left);
+
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
